4e904abec72d65cfccd665bb75f2ebe18060cd5e
agim221 <muhamadagim221@gmail.com>
2023-02-24 15:49:40 +0700

    update hover

--- beautifulSoap.py
+++ beautifulSoap.py
@@ -11,11 +11,11 @@ latest = soup.find(class_="box feed")
 title = latest.find_all("h2", class_="title") 
 category = latest.find_all("span", class_="kanal")
 picture = latest.find_all('img')
-
+link = latest.find_all('article')
 
 result = []
 for i in range(len(title)):
-    result.append({"id":i+1, "judul": title[i].text.strip(), "kategori":category[i - int(1)].text.strip(), "picture":str(picture[i]).split('"')[3]})
+    result.append({"id":i+1, "judul": title[i].text.strip(), "kategori":category[i - int(1)].text.strip(), "picture":str(picture[i]).split('"')[3], "link":str(link[i]).split('"')[15]})
 
 HasilJSON = json.dumps(result)
 JSONFile = open("BeritaTerkini.json", "w")
