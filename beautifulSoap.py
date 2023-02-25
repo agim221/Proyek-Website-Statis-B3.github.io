59d9e749744b57f9f9c0eaf1d0aa534fcdf2b023
agim221 <muhamadagim221@gmail.com>
2023-02-25 20:04:02 +0700

    update tanggal

--- beautifulSoap.py
+++ beautifulSoap.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 URL = "https://www.cnnindonesia.com/olahraga"
 page = requests.get(URL)
@@ -8,6 +9,7 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 latest = soup.find(class_="box feed")
 
+now =datetime.now()
 title = latest.find_all("h2", class_="title") 
 category = latest.find_all("span", class_="kanal")
 picture = latest.find_all('img')
@@ -15,7 +17,8 @@ link = latest.find_all('article')
 
 result = []
 for i in range(len(title)):
-    result.append({"id":i+1, "judul": title[i].text.strip(), "kategori":category[i - int(1)].text.strip(), "picture":str(picture[i]).split('"')[3], "link":str(link[i]).split('"')[15]})
+    print(latest);
+    result.append({"id":i+1, "judul": title[i].text.strip(), "kategori":category[i - int(1)].text.strip(), "picture":str(picture[i]).split('"')[3], "link":str(link[i]).split('"')[15], "waktu_scraping": now.strftime("%Y-%m-%d %H:%M:%S")})
 
 HasilJSON = json.dumps(result)
 JSONFile = open("BeritaTerkini.json", "w")
---
+++ webscrapping/WebScrappingSelenium.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+import urllib.request
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+#PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome()
+
+driver.get("https://rateyourmusic.com/charts/top/album/all-time/")
+time.sleep(5)
+
+music_list = []
+i = 1
+print("Adding Top 80 Albums...")
+while i <= 80:
+    for music in driver.find_elements(By.CLASS_NAME, "page_section_charts_item_wrapper"):
+        driver.execute_script("window.scrollBy(0, 400);")
+        time.sleep(0.8)
+        for img in music.find_elements(By.TAG_NAME, "img"):
+            
+            urllib.request.urlretrieve(img.get_attribute("src"), str(i)+".png")
+            
+            music_list.append(
+                {"No": i ,
+                 "Judul":music.text.split("\n")[0],
+                 "Penyanyi": music.text.split("\n")[1],
+                 "Rating": music.text.split("\n")[2],
+                 "Genre": music.text.split("\n")[6].split()[0],
+                 "Tanggal_Rilis": music.text.split("\n")[5],
+                 "Img": img.get_attribute("src")
+                 }
+                )
+            i = i + 1
+            
+            break
+    try:
+        pagination_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="ui_pagination_page_charts_navigation_bottom"]/a'))
+        )
+        pagination_btn.click()
+
+        time.sleep(10)
+    except NoSuchElementException as e:
+        break
+    
+hasil_scraping = open("hasilscraping.json", "w")
+json.dump(music_list, hasil_scraping, indent = 6)
+hasil_scraping.close()
+
+driver.quit()
